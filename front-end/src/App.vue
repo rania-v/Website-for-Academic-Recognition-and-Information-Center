06acb6337ef62f7cbf9e5e37780968ff75d8355f
rania-v <rania-98@outlook.com>
2021-12-24 14:15:41 +0200

    first commit

---
+++ front-end/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <v-app-bar app :color=base_color dark height="120px">
+      <v-row>
+        <v-col cols="3" align-self="center">
+          <router-link to="/">
+            <v-img v-bind:src=logo ></v-img>
+          </router-link>
+        </v-col>
+        <v-col>
+          <v-row>
+          <v-spacer></v-spacer>
+            <v-col cols="6" align-self="center" align="center">
+              <v-btn rounded small class="grey lighten-1 ma-2">Sign Up</v-btn>
+              <v-btn rounded small class="ma-2">Sign IN</v-btn>
+            </v-col>
+            <v-col align-self="center" align="center">
+              <v-btn rounded x-small :text="eng" depressed :class="{'ma-2 deep-orange': !eng}" v-on:click="eng=false">Ελληνικά</v-btn>
+            </v-col>
+            <v-divider vertical></v-divider>
+            <v-col align-self="center" align="center">
+              <v-btn rounded x-small :text="!eng" depressed :class="{'ma-2 deep-orange': eng}" v-on:click="eng=true">English</v-btn>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-spacer></v-spacer>
+            <v-col  class="ma-0 pa-0 cyan">
+              <v-tabs  class="cyan ma-0 pb-0 white--text">
+                <v-tabs-slider color="cyan darken-3"></v-tabs-slider>
+                  <v-tab v-for="item in Sections" :key="item.route" class="cyan" :to="item.route">{{item.name}}</v-tab>
+              </v-tabs>
+            </v-col>
+          </v-row>
+        </v-col>
+          
+      </v-row>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: function(){
+    return {
+      logo: require("./assets/logo_doatap.png"),
+      base_color: "cyan",
+      eng: false,
+    Sections: [ {name: "Αρχική", route: "/"},
+                {name: "Αναγνώριση", route: "/Anagnwrish"},
+                { name: "Αίτηση", route: "/Aithsh"},
+                { name: "Ενημέρωση", route: "/Enhmerwsh"},
+                { name: "Επικοινωνία", route: "/Epikoinwnia"}]
+    }
+  }
+};
+</script>
---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-card class="pl-3 pr-3" flat color="cyan" dark>
+      <v-select :items='country'></v-select>
+      <v-card-title>Δες Αν Αναγνωρίζεται το Πτυχίο σου!</v-card-title>
+      <v-divider></v-divider>
+      <v-row>
+        <v-col cols="8">
+          <v-card-title style="font-family:sans-serif; word-break: break-word;">Δες αν μπορείς να εκδώσεις πσιτοποιητικό αναγνώρισης ή επαλήθευσης γνησιότητας αυτόματα χωρίς να κάνεις Αίτηση!</v-card-title>
+        </v-col>
+        <v-col align-self="center" class="d-flex justify-center">
+          <v-btn outlined rounded>Έλεγχος</v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+    data : function(){
+      return{
+        country: {
+          
+        }
+      }
+    }
+  }
+</script>
